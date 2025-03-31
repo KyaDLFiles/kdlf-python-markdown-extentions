487817eca80d317252bd98e69e2c3e0560014fb5
KyaDLFiles <103515463+KyaDLFiles@users.noreply.github.com>
2025-03-31 16:18:13 +0200

    Rowspan (messy code)

--- extended_tables_extension.py
+++ extended_tables_extension.py
@@ -13,13 +13,15 @@ ROW_CLASS_PATTERN = r'\+\.[_\-a-zA-Z][_\-a-zA-Z\d]*'
 CELL_CLASS_PATTERN = r'(?<!\+)\.[_\-a-zA-Z][_\-a-zA-Z\d]*'
 ROW_HIGHLIGHT_PATTERN = r'\+!\d+'
 CELL_HIGHLIGHT_PATTERN = r'(?<!\+)!\d+'
-COLSPAN_PATTERN = r'>\d+'
+COLSPAN_PATTERN = r'\>\d+'
+ROWSPAN_PATTERN = r'\^\d+'
 PROPERTIES_PATTERN = (r'^!{ *'
                       f'({ROW_CLASS_PATTERN} *)*' # Group 1
                       f'({CELL_CLASS_PATTERN} *)*' # Group 2
                       f'({ROW_HIGHLIGHT_PATTERN} *)*' # Group 3
                       f'({CELL_HIGHLIGHT_PATTERN} *)*' # Group 4
                       f'({COLSPAN_PATTERN} *)?' # Group 5
+                      f'({ROWSPAN_PATTERN} *)?' # Group 6
                       r'}')
 
 class ExtendedTableProcessor(tables.TableProcessor):
@@ -30,6 +32,40 @@ class ExtendedTableProcessor(tables.TableProcessor):
     # So I've just kept them in a fixed order, I don't think anyone would find it too much of an issue
     # (if anyone else apart from me were ever to use this extension, that is)
     # (also, it's definitely possible to have the flags in any order by just using iteration)
+    def run(self, parent: etree.Element, blocks: list[str]):
+        # Unfortunately I had to copy the whole function because I can't intercept len(align) otherwise
+        """ Parse a table block and build table. """
+        block = blocks.pop(0).split('\n')
+        header = block[0].strip(' ')
+        rows = [] if len(block) < 3 else block[2:]
+
+        # Get alignment of columns
+        align: list[str | None] = []
+        for c in self.separator:
+            c = c.strip(' ')
+            if c.startswith(':') and c.endswith(':'):
+                align.append('center')
+            elif c.startswith(':'):
+                align.append('left')
+            elif c.endswith(':'):
+                align.append('right')
+            else:
+                align.append(None)
+
+        self.rowspans = [0] * len(align) # Initialize rowspans
+
+        # Build table
+        table = etree.SubElement(parent, 'table')
+        thead = etree.SubElement(table, 'thead')
+        self._build_row(header, thead, align)
+        tbody = etree.SubElement(table, 'tbody')
+        if len(rows) == 0:
+            # Handle empty table
+            self._build_empty_row(tbody, align)
+        else:
+            for row in rows:
+                self._build_row(row.strip(' '), tbody, align)
+
     def _build_row(self, row: str, parent: etree.Element, align: Sequence[str | None]) -> None:
         """ Given a row of text, build table cells. """
         tr = etree.SubElement(parent, 'tr')
@@ -40,53 +76,84 @@ class ExtendedTableProcessor(tables.TableProcessor):
         # We use align here rather than cells to ensure every row
         # contains the same number of columns.
 
-        i = 0 # We need to be able to increment the index separately due to colspans
+        i = 0 # "actual" index of the cell (if you consider colspanned cells as separate)
+        j = 0 # index of cell as being read from the file
         while i < len(align):
-            a = align[i] # Get the align value for this column
-            c = etree.SubElement(tr, tag) # Create the tr
-            try:
-                text = cells[i] # Get the text inside the cell WITHOUT stripping
-                m = re.match(PROPERTIES_PATTERN, text) # Try to match with the properties at the beginning (right after the pipe)
-                if m:
-                    if m.group(1) is not None: # If the first group matched, it matched with +.
-                        if 'class' not in tr.attrib:
-                            tr.attrib['class'] = ''
-                        for m2 in re.finditer(ROW_CLASS_PATTERN, text): # Get the highlight number
-                            tr.attrib['class'] = f"{tr.attrib['class']} {m2.group(0)[2:]}" # Apply class
+            if self.rowspans[i] == 0:
+                a = align[i] # Get the align value for this column
+                c = etree.SubElement(tr, tag) # Create the td
+                colspan = 1
+                rowspan = 1
+                try:
+                    text = cells[j] # Get the text inside the cell WITHOUT stripping
+                    m = re.match(PROPERTIES_PATTERN, text) # Try to match with the properties at the beginning (right after the pipe)
+                    if m:
+                        if m.group(1) is not None: # If the first group matched, it matched with +.
+                            if 'class' not in tr.attrib:
+                                tr.attrib['class'] = ''
+                            for m2 in re.finditer(ROW_CLASS_PATTERN, text): # Get the highlight number
+                                tr.attrib['class'] = f"{tr.attrib['class']} {m2.group(0)[2:]}" # Apply class
 
-                    if m.group(2) is not None: # If the second group matched, it matched with .
-                        if 'class' not in c.attrib:
-                            c.attrib['class'] = ''
-                        for m2 in re.finditer(CELL_CLASS_PATTERN, text):  # Get the highlight number
-                            c.attrib['class'] = f"{c.attrib['class']} {m2.group(0)[1:]}"  # Apply class
+                        if m.group(2) is not None: # If the second group matched, it matched with .
+                            if 'class' not in c.attrib:
+                                c.attrib['class'] = ''
+                            for m2 in re.finditer(CELL_CLASS_PATTERN, text):  # Get the highlight number
+                                c.attrib['class'] = f"{c.attrib['class']} {m2.group(0)[1:]}"  # Apply class
 
-                    if m.group(3) is not None: # If the third group matched, it matched with +!
-                        if 'class' not in tr.attrib:
-                            tr.attrib['class'] = ''
-                        for m2 in re.finditer(ROW_HIGHLIGHT_PATTERN, text): # Get the highlight number
-                            tr.attrib['class'] = f"{tr.attrib['class']} table-highlight-{m2.group(0)[2:]}" # Apply class
+                        if m.group(3) is not None: # If the third group matched, it matched with +!
+                            if 'class' not in tr.attrib:
+                                tr.attrib['class'] = ''
+                            for m2 in re.finditer(ROW_HIGHLIGHT_PATTERN, text): # Get the highlight number
+                                tr.attrib['class'] = f"{tr.attrib['class']} table-highlight-{m2.group(0)[2:]}" # Apply class
 
-                    if m.group(4) is not None: # If the fourth group matched, it matched with !
-                        if 'class' not in c.attrib:
-                            c.attrib['class'] = ''
-                        for m2 in re.finditer(CELL_HIGHLIGHT_PATTERN, text):  # Get the highlight number
-                            c.attrib['class'] = f"{c.attrib['class']} table-highlight-{m2.group(0)[1:]}"  # Apply class
+                        if m.group(4) is not None: # If the fourth group matched, it matched with !
+                            if 'class' not in c.attrib:
+                                c.attrib['class'] = ''
+                            for m2 in re.finditer(CELL_HIGHLIGHT_PATTERN, text):  # Get the highlight number
+                                c.attrib['class'] = f"{c.attrib['class']} table-highlight-{m2.group(0)[1:]}"  # Apply class
 
-                    if m.group(5) is not None: # If the third group matched, it matched with >
-                        colspan = re.search(COLSPAN_PATTERN, text).group(0)[1]
-                        c.attrib['colspan'] = colspan # Apply colspan
-                        i += int(colspan) - 1 # Add colspan-1 to the counter, skipping colspan-1 cells
+                        if m.group(5) is not None: # If the fifth group matched, it matched with >
+                            colspan = int(re.search(COLSPAN_PATTERN, text).group(0)[1])
+                            c.attrib['colspan'] = str(colspan) # Apply colspan
 
-                c.text = re.sub(PROPERTIES_PATTERN, '', text).strip() # Remove the properties, strip, and set as the text of the cell
-            except IndexError:  # pragma: no cover
-                c.text = "" # Create an extra empty cell if there aren't enough in the row
-            finally:
-                i += 1 # Increment counter
-            if a:
-                if self.config['use_align_attribute']:
-                    c.set('align', a)
-                else:
-                    c.set('style', f'text-align: {a};')
+                        if m.group(6) is not None: # If the sixth group matched, it matched with ^
+                            rowspan = int(re.search(ROWSPAN_PATTERN, text).group(0)[1])
+                            c.attrib['rowspan'] = str(rowspan)
+
+                        self.rowspans[i] = rowspan - 1 # Set the rowspan for this cell
+                        if rowspan > 1 and colspan > 1: # If the cell has both rowspan and colspan, set dummy rowspans for next cells
+                            for k in range(colspan - 1):
+                                self.rowspans[k + i + 1] = -1
+                        if colspan > 1: # Skip n-1 cells if cell has n colspan
+                            i += colspan - 1
+
+
+
+                    c.text = re.sub(PROPERTIES_PATTERN, '', text).strip() # Remove the properties, strip, and set as the text of the cell
+                except IndexError:  # pragma: no cover
+                    c.text = "" # Create an extra empty cell if there aren't enough in the row
+                finally:
+                    j += 1
+                if a:
+                    if self.config['use_align_attribute']:
+                        c.set('align', a)
+                    else:
+                        c.set('style', f'text-align: {a};')
+            else: # if there's a cell in a previous row that has a rowspan overriding this cell, don't generate anything and decrement the counter
+                k = i + 1 # Index starting on next cell
+                j += 1 # Skip dummy cell in markdown
+                clear = False
+                if self.rowspans[i] == 1: # If current rowspan is 1, clear all dummy values on the right
+                    clear = True
+
+                self.rowspans[i] -= 1 # Decrease current rowspan
+                while k < len(align) and self.rowspans[k] == -1:
+                    i += 1 # Skip one cell
+                    if clear:
+                        self.rowspans[k] = 0
+                    k += 1
+
+            i += 1
 
 class ExtendedTableExtension(Extension):
     """ Add tables to Markdown, with the following extensions:
@@ -97,6 +164,7 @@ class ExtendedTableExtension(Extension):
     +!<number> : Apply class table-highlight-<number> to row
     !<number>  : Apply class table-highlight-<number> to cell
     ><number>  : Apply colspan of <number> to cell
+    ^<number>  : Apply rowspan of <number> to cell
 
     The options must be specified in this order, but multiple values for the same attribute can be applied (except for colspan), and can be separated by spaces
     Example:
@@ -108,16 +176,55 @@ class ExtendedTableExtension(Extension):
             ...
         </tr>
 
-    NOTE: when applying a colspan of X, the X cells after it will be discarded (so they may as well be empty)
+    NOTE: when applying a rowspan of X, the X cells below that cell will be discarded (so they may as well be empty)
     Example:
-        |!{>4} Span|These|will|be|discarded|but not this one|
+        |!{^5} Span   |A|
+        |These        |B|
+        |Will         |C|
+        |Be           |D|
+        |Discared     |E|
+        |But not this |F|
     Creates:
-        <tr class="table-highlight-1">
-            <td colspan="4">Span</td>
-            <td>but not this one</td>
+        <tr>
+            <td rowspan="5">Span</td>
+            <td>A</td>
+        </tr>
+        <tr>
+            <td>B</td>
+        </tr>
+        <tr>
+            <td>C</td>
+        </tr>
+        <tr>
+            <td>D</td>
+        </tr>
+        <tr>
+            <td>E</td>
+        </tr>
+        <tr>
+            <td>But not this</td>
+            <td>E</td>
         </tr>
 
-    This extensions inherits from TableProcessor/TableExtension, and only modifies the _build_row function
+    The same will NOT happen with colspan
+    Example:
+        |!{>3}Span|After span|
+        |A|B|C    |D         |
+    Creates:
+        <tr>
+            <td colspan="3">Span</td>
+            <td>After span</td>
+        </tr>
+        <tr>
+            <td>A</td>
+            <td>B</td>
+            <td>C</td>
+            <td>D</td>
+        </tr>
+
+    IMPORTANT: malformed tables (eg: having cells with a colspan and rowspan that are overlapping) will generate unexpected results
+
+    This extensions inherits from TableProcessor/TableExtension, and only modifies the _build_row and run() functions
     """
 
     def __init__(self, **kwargs):
