1004238a2cc891a1e6f29a5a523356db28acebc2
KyaDLFiles <103515463+KyaDLFiles@users.noreply.github.com>
2025-04-01 10:54:38 +0200

    Superscript

--- highlight_extensions.py
+++ highlight_extensions.py
@@ -1,5 +1,6 @@
 from markdown.inlinepatterns import InlineProcessor
 from markdown.extensions import Extension
+from markdown import Markdown
 import xml.etree.ElementTree as etree
 
 class WarningHighlightProcessor(InlineProcessor):
@@ -12,20 +13,15 @@ class WarningHighlightProcessor(InlineProcessor):
 
 class WarningHighlightExtension(Extension):
     """Extension to convert *! this pattern !* to <span style='text-warning'> this pattern </span>"""
+
     def extendMarkdown(self, md):
-        WARNING_HIGHLIGHT_PATTERN = r"\*!(.*?)!\*" # match *! a pattern like this *!
+        WARNING_HIGHLIGHT_PATTERN = r"\*!(.*?)!\*"  # match *! a pattern like this *!
 
         md.registerExtension(self)
         self.md = md
         md.inlinePatterns.register(WarningHighlightProcessor(WARNING_HIGHLIGHT_PATTERN, md), "warning_highlight", 65)
 
-class UnsureHighlightProcessor(InlineProcessor):
-    SPAN_CLASS = "text-unsure"
-    def handleMatch(self, m, data):
-        el = etree.Element('span')
-        el.text = m.group(1)
-        el.attrib["class"] = self.SPAN_CLASS
-        return el, m.start(0), m.end(0)
+
 
 class UnsureHighlightExtension(Extension):
     """Extension to convert *! this pattern !* to <span style='text-unsure'> this pattern </span>"""
@@ -36,3 +32,30 @@ class UnsureHighlightExtension(Extension):
         self.md = md
         md.inlinePatterns.register(UnsureHighlightProcessor(UNSURE_HIGHLIGHT_PATTERN, md), "unsure_highlight", 66)
 
+    def __init__(self, **kwargs):
+        self.config = {
+            'tooltip': ["", "Tooltip to show when hovering on the superscript"],
+        }
+        """ Default configuration options. """
+
+        super().__init__(**kwargs)
+
+
+class UnsureHighlightProcessor(InlineProcessor):
+    SPAN_CLASS = "text-unsure"
+
+    def handleMatch(self, m, data):
+        el = etree.Element('span')
+        el.text = m.group(1)
+        el.attrib["class"] = self.SPAN_CLASS
+        e2 = etree.SubElement(el, 'sup')
+        e2.text = "[?]"
+        if self.tooltip != "":
+            e2.attrib["title"] = self.tooltip
+        return el, m.start(0), m.end(0)
+
+    def __init__(self, pattern: str, md: Markdown, ext: UnsureHighlightExtension):
+        super().__init__(pattern)
+        self.md = md
+
+        self.tooltip = ext.getConfig("tooltip")
